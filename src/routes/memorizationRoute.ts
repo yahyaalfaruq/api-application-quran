fb0b22ffb41b6923ed09937a360bc226d5c96d55
jhon <165111230+yahyaalfaruq@users.noreply.github.com>
2024-12-04 10:16:00 +0700

    edit in memorizationRoute.ts

--- src/routes/memorizationRoute.ts
+++ src/routes/memorizationRoute.ts
@@ -7,6 +7,7 @@ import dotenv from "dotenv";
 dotenv.config();
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
 
 const router = Router();
